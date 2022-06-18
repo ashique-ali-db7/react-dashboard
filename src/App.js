1c19f14156425ba3d8e4cbc50ba1a0f118bcf6e9
Ashique Ali <ashiquealikmvkd@gmail.com>
2022-06-18 13:58:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{useEffect} from 'react';
+import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {FiSettings} from 'react-icons/fi';
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+const App = () => {
+  return (
+<div>
+<BrowserRouter>
+<div></div>
+</BrowserRouter>
+</div>  )
+}
+
+export default App
